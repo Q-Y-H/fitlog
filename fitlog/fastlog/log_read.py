aa8a43e56ea489e0dec3b125aecec2ba26b54e33
Q-Y-H <qyhfrank@gmail.com>
2020-12-20 22:54:56 +0800

    Fit logdir config with runx

--- fitlog/fastlog/log_read.py
+++ fitlog/fastlog/log_read.py
@@ -12,7 +12,7 @@ class LogReader:
     """
     用于读取日志的类, 用于配合Table使用
     """
-    
+
     def __init__(self):
         """
         self._line_counter里面的内容:{save_log_dir: {filename: (line_count, last_change_time)}}
@@ -20,7 +20,7 @@ class LogReader:
         self._log_dir = None
         self._ignore_null_loss_or_metric = True  # 如果loss和metric都是null的话，则忽略
         self._line_counter = defaultdict(lambda: None)  # 记住每个log读取到的line的数量以及修改时间
-    
+
     def set_log_dir(self, log_dir: str):
         """
         设置 log 的存放位置
@@ -33,10 +33,10 @@ class LogReader:
                 empty = False
         if empty:
             raise RuntimeError("`{}` has no valid logs.".format(log_dir))
-        
+
         self._log_dir = log_dir
         self._line_counter.clear()  # 删除记录，使得重新读取
-    
+
     def read_logs(self, ignore_log_names: dict = None) -> List[dict]:
         """
         从日志存放路径读取日志. 只会读取有更新的log
@@ -90,16 +90,16 @@ def _read_save_log(_save_log_dir: str, ignore_null_loss_or_metric: bool = True, 
     """
     给定一个包含metric.log, hyper.log, meta.log以及other.log的文件夹，返回一个包含数据的dict. 如果为null则返回空字典
     不读取loss.log, 因为里面的内容对table无意义。
-    
+
     :param _save_log_dir: 日志存放的目录， 已经最后一级了，即该目录下应该包含metric.log等了
     :param ignore_null_loss_or_metric: 是否忽略metric和loss都为空的文件
     :param file_stats::
-    
+
             {
                 'meta.log': [current_line, last_modified_time],
                 'hyper.log':[], 'metric.log':[], 'other.log':[]
             }
-            
+
     :return:
         _dict: {'metric': {nested dict}, 'loss': {} }
         file_stats: {'meta.log': [current_line, last_modified_time],
@@ -135,7 +135,7 @@ def _read_save_log(_save_log_dir: str, ignore_null_loss_or_metric: bool = True, 
                         f2.write(best_line)
 
         empty = _is_file_empty('best_metric.log') and _is_file_empty('loss.log') and _is_file_empty('metric.log')
-        
+
         if empty and ignore_null_loss_or_metric:
             return _dict, file_stats
 
@@ -159,7 +159,7 @@ def _read_save_log(_save_log_dir: str, ignore_null_loss_or_metric: bool = True, 
 def is_log_dir_has_step(_save_log_dir: str, check_files=('metric.log', 'loss.log')) -> bool:
     """
     给定log_dir, 判断是否有step数据
-    
+
     :param _save_log_dir 日志存放的目录
     :param check_files: 检查file是否含有step
     :return: 是否有step数据
@@ -185,7 +185,7 @@ def is_log_dir_has_step(_save_log_dir: str, check_files=('metric.log', 'loss.log
 def _read_nonstep_log_file(filepath: str, start_line: int = 0) -> (dict, int):
     """
     给定一个filepath, 读取里面非Step: 开头的line，每一行为json，使用后面的内容覆盖前面的内容
-    
+
     :param filepath: 读取文件的路径
     :param start_line: 从哪一行开始读取
     :return: 返回一个字典(没有内容为空)和最后读取到的行号
@@ -210,7 +210,7 @@ def _read_nonstep_log_file(filepath: str, start_line: int = 0) -> (dict, int):
 def merge(a: dict, b: dict, use_b: bool = True) -> dict:
     """
     将两个dict recursive合并到a中，有相同key的，根据use_b判断使用哪个值
-    
+
     :param a: 字典 a
     :param b: 字典 b
     :param use_b: 是否使用字典 b 的值
@@ -230,26 +230,27 @@ def merge(a: dict, b: dict, use_b: bool = True) -> dict:
 def is_dirname_log_record(dir_path: str) -> bool:
     """
     检查dir_path是否是一个合法的log目录。合法的log目录里必须包含meta.log。
-    
+
     :param dir_path: 被检测的路径
     :return: 是否合法
     """
     if not os.path.isdir(dir_path):
         return False
-    if len(re.findall(r'log_\d{8}_\d{6}$', dir_path)) != 0:
-        filenames = ['meta.log']  # 至少要有meta.log表明这个是合法的log
-        for filename in filenames:
-            if not os.path.exists(os.path.join(dir_path, filename)):
-                return False
-        return True
-    else:
-        return False
+    # if len(re.findall(r'log_\d{8}_\d{6}$', dir_path)) != 0:
+    #     filenames = ['meta.log']  # 至少要有meta.log表明这个是合法的log
+    #     for filename in filenames:
+    #         if not os.path.exists(os.path.join(dir_path, filename)):
+    #             return False
+    #     return True
+    # else:
+    #     return False
+    return True
 
 
 def is_log_record_finish(save_log_dir: str) -> bool:
     """
     检测日志的记录是否已经结束
-    
+
     :param save_log_dir: 日志存放的目录
     :return:
     """
@@ -271,39 +272,39 @@ def is_log_record_finish(save_log_dir: str) -> bool:
 class StandbyStepLogReader(threading.Thread):
     """
     用于多线程读取日志的类. 配合画图使用的。
-    
+
     :param save_log_dir: 日志存放的目录
     :param uuid: 用于唯一识别 Reader 的 uuid
     :param wait_seconds:  在文件关闭后再等待{wait_seconds}秒结束进程
     :param max_no_updates: 在{max_no_updates}次都没有更新时结束进程
     """
-    
+
     def __init__(self, save_log_dir: str, uuid: str, wait_seconds: int = 60, max_no_updates: int = 30):
         super().__init__()
-        
+
         self.save_log_dir = save_log_dir
         self._file_handlers = {}
-        
+
         self.uuid = uuid
         self._last_access_time = time.time()
         # 如果这么长时间没有读取到新的数据，就认为是不需要再读取的了
         # 如果这么长时间没有再次调用，就关掉文件
         self._wait_seconds = wait_seconds
-        
+
         self.unfinish_lines = {}  # 防止读写冲突, key: line
         self._stop_flag = False
         self._quit = False
         self._no_update_count = 0
         self.max_no_update = max_no_updates
-        
+
         self._last_meta_md_time = None
         self._meta_path = os.path.join(self.save_log_dir, 'meta.log')
         self._total_steps = None
-    
+
     def _create_file_handler(self, filenames=('metric.log', 'loss.log')):
         """
         检查是否有未加入的handler，有则加入进来
-        
+
         :return:
         """
         for filename in filenames:
@@ -313,14 +314,14 @@ class StandbyStepLogReader(threading.Thread):
             filepath = os.path.join(self.save_log_dir, filename)
             handler = open(filepath, 'r', encoding='utf-8')
             self._file_handlers[handler_name] = handler
-    
+
     def _is_finish_in_meta(self) -> bool:
         """
         检查是否已经在meta中写明了finish的状态了
-        
+
         :return: bool
         """
-        
+
         last_meta_md_time = os.path.getmtime(self._meta_path)
         if self._last_meta_md_time is None or self._last_meta_md_time != last_meta_md_time:
             with open(self._meta_path, 'r', encoding='utf-8') as f:
@@ -337,12 +338,12 @@ class StandbyStepLogReader(threading.Thread):
                         pass
         self._last_meta_md_time = last_meta_md_time
         return False
-    
+
     @staticmethod
     def read_update_single_log(filepaths: List[str], ranges: dict) -> dict:
         """
         调用这个函数，获取filepaths中满足range_min, range_max的log
-        
+
         :param filepaths: 完整的path路径
         :param ranges: {'metric':[min, max] }
         :return: 返回值的结构如下。loss这个list是进行了step排序的
@@ -350,14 +351,14 @@ class StandbyStepLogReader(threading.Thread):
                 loss: [dict('step':x, epoch:value, 'loss':{'loss1':xx})],
                 metric:[dict('step':x, epoch:value, 'metric':{'SpanFMetric':{'f':xx}})]
             }
-                
+
         """
         updates = defaultdict(list)
         for filepath in filepaths:
             filename = os.path.basename(filepath).split('.')[0]
             range_min = int(ranges[filename][0])
             range_max = int(ranges[filename][1])
-            
+
             with open(filepath, 'r', encoding='utf-8') as f:
                 for line in f:
                     if not line.endswith('\n'):  # 结尾不是回车，说明没有读完
@@ -375,11 +376,11 @@ class StandbyStepLogReader(threading.Thread):
                 if filename in updates and len(updates[filename]) != 0:  # 对step排序，保证不要出现混乱
                     updates[filename].sort(key=lambda x: x['step'])
         return updates
-    
+
     def read_update(self, only_once: bool = False, handler_names=('metric', 'loss')) -> dict:
         """
         调用这个函数，获取新的更新。如果第一次调用则是读取到当前所有的记录。
-        
+
         :param only_once: 是否只读取内容一次。是的话就不会保持读取到的行数，之后直接退出了
         :param handler_names: 只check包含在handler_name的内容
         :return: 返回值的结构如下
@@ -389,7 +390,7 @@ class StandbyStepLogReader(threading.Thread):
                 finish:bool(not every time),
                 total_steps:int(only the first access)
             }
-                
+
         """
         updates = {}
         if not self._quit:
@@ -445,19 +446,19 @@ class StandbyStepLogReader(threading.Thread):
         if 'finish' in updates:
             self._quit = True
             self.stop()
-        
+
         return updates
-    
+
     def _close_file_handler(self):
         for key in list(self._file_handlers.keys()):
             handler = self._file_handlers[key]
             handler.close()
         self._file_handlers.clear()
-    
+
     def stop(self):
         """
         如果手动停止某个任务
-        
+
         :return:
         """
         self._stop_flag = True
@@ -468,11 +469,11 @@ class StandbyStepLogReader(threading.Thread):
             if count > 3:
                 raise RuntimeError("Multi-thread bug here. It should not run twice.")
             count += 1
-    
+
     def run(self):
         """
         重载了多线程的运行函数
-        
+
         :return:
         """
         while time.time() - self._last_access_time < self._wait_seconds and not self._stop_flag and \
